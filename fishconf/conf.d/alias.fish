9f44044b8f8e558ac1eb15cb052f00177bd3e7a7
InvalidName662 <54070204+InvalidName662@users.noreply.github.com>
2021-10-18 00:09:53 +0200

    Initial commit

---
+++ fishconf/conf.d/alias.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+  alias 'sdown'='systemctl poweroff'
+  alias 'sudo'='command sudo -v; command sudo'
+  alias 'gaytrain'='command sl | command lolcat && clear'
+  alias 'nmcu'='nodemcu-tool'
+  alias 'fishconf'='$EDITOR ~/.config/fish/config.fish'
+end
+
