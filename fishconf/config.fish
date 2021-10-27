4a40d28962f50c779b405265cd86fc92cd2e395f
InvalidName662 <54070204+InvalidName662@users.noreply.github.com>
2021-10-27 22:58:01 +0200

    vi bindings for fish and tmux

--- fishconf/config.fish
+++ fishconf/config.fish
@@ -1,9 +1,12 @@
 if status is-interactive
+	# vim all the way
+	fish_vi_key_bindings
+
 	# Commands to run in interactive sessions can go here
 	starship init fish | source
 
+	if command -v thefuck &> /dev/null
+		thefuck --alias | source
+	end
 end
 
-if command -v thefuck &> /dev/null
-	thefuck --alias | source
-end
