517c1bbf25d7b6a4f813e4e5dd590a0b27d41e65
InvalidName662 <54070204+InvalidName662@users.noreply.github.com>
2022-03-06 13:39:40 +0100

    sudo didnt work :(

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
+#Disable Greeting
+set fish_greeting
+
 #ENVIRONMENT VARIABLES
 set -x EDITOR nvim
 
+#interactive mode only
 if status is-interactive
 	# vim all the way
 	fish_vi_key_bindings
@@ -17,39 +21,37 @@ if status is-interactive
 	if command -v starship &> /dev/null && not set -q LINUXTERM
 		starship init fish | source
 	end
-
-	if command -v thefuck &> /dev/null
-		thefuck --alias | source
-	end
-
-	# aliases
-	alias 'sdown'='shutdown -h now'
-	alias 'nmcu'='nodemcu-tool' # will propably never use it again but this stays
-	alias 'fishconf'='$EDITOR ~/.config/fish/config.fish'
-	alias 'cat'='bat'
-	alias 'la'='ls -A'
-	alias 'lsblk'='command lsblk -f'
-	alias ':q'='exit' # done that way to often
-
-	# git shorts
-	alias 'gs'='git status'
-	alias 'gp'='git push'
-	
-	#kitty ssh fix
-	if [ $TERM = 'xterm-kitty' ]
-			alias 'ssh'='kitty +kitten ssh'
-	end
-
-	if command -v tutel &> /dev/null 
-		tutel nav init fish | source
-	end
-
-	#man with color
-	if command -v batman &> /dev/null 
-		alias 'man'='batman'
-	end
-
-	#Disable Greeting
-	set fish_greeting
 end
+
+#kitty ssh fix
+if [ $TERM = 'xterm-kitty' ]
+		alias 'ssh'='kitty +kitten ssh'
+end
+
+if command -v thefuck &> /dev/null
+	thefuck --alias | source
+end
+
+# aliases
+alias 'sdown'='shutdown -h now'
+alias 'nmcu'='nodemcu-tool' # will propably never use it again but this stays
+alias 'fishconf'='$EDITOR ~/.config/fish/config.fish'
+alias 'cat'='bat'
+alias 'la'='ls -A'
+alias 'lsblk'='command lsblk -f'
+alias ':q'='exit' # done that way to often
+
+# git shorts
+alias 'gs'='git status'
+alias 'gp'='git push'
+
+if command -v tutel &> /dev/null 
+	tutel nav init fish | source
+end
+
+#man with color
+if command -v batman &> /dev/null 
+	alias 'man'='batman'
+end
+
 
--- fish/functions/sudo.fish
+++
@@ -1,6 +0,0 @@
-function sudo
-    if functions -q $argv[1]
-        set argv fish -c "$argv"
-    end
-    command sudo $argv
-end
