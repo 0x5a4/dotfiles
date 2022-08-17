acfffffaa5bc18fd4cec7238bef2ade7a095bc8a
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2022-08-18 01:34:19 +0200

    tmux: smart window naming

--- tmux/pane_title.sh
+++ tmux/pane_title.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $1
-
-basename "$(git rev-parse --show-toplevel 2> /dev/null || pwd)"
+current_path="$(git rev-parse --show-toplevel 2> /dev/null || pwd)"
+echo $current_path | rev | cut -d"/" -f1-2 | rev
