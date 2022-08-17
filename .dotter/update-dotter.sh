df11a23e32f749695497b066d36a9b64fe073e3e
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2022-08-17 19:25:39 +0200

    remove dotter from repo

--- .dotter/update-dotter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET=$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")
-
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/SuperCuber/dotter/releases/latest | grep browser_download_url | grep dotter\" | cut -d '"' -f 4)
-
-curl -s -L -o "$TARGET"/dotter "${DOWNLOAD_URL}"
-
-chmod +x "$TARGET"/dotter
