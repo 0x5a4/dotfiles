2c5b726ab5c1cede59b3e18b49c11fee9a91e3ef
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2022-10-28 18:01:47 +0200

    fish: no vim bindings :(

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,6 @@ set fish_greeting
 #interactive mode only
 if status is-interactive
 	# vim all the way
-	fish_vi_key_bindings
 	set fish_cursor_default block
 	set fish_cursor_insert block
 	set fish_cursor_replace_one underscore
