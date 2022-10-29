fbd10476a3e94d3d11d8e722b1e8c98f5a3501b0
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2022-10-29 13:33:38 +0200

    update readme

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ set fish_greeting
 
 #interactive mode only
 if status is-interactive
-	# vim all the way
 	set fish_cursor_default block
 	set fish_cursor_insert block
 	set fish_cursor_replace_one underscore
