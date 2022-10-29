8ab4bea7e2f78b628991ef8be70743b262ebb5cf
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2022-10-30 01:07:37 +0200

    fish: equery shortcuts

---
+++ fish/conf.d/gentoo.fish
@@ -0,0 +1,23 @@
+if not command -v emerge 2&> /dev/null
+    return
+end
+
+# emerge
+alias emerge='command emerge --verbose $argv'
+
+# equery
+alias eq='command equery $argv'
+alias eqb='command equery belongs $argv'
+alias eqc='command equery changes $argv'
+alias eqk='command equery check $argv'
+alias eqd='command equery depends $argv'
+alias eqg='command equery depgraph $argv'
+alias eqf='command equery files $argv'
+alias eqa='command equery has $argv'
+alias eqh='command equery hasuse $argv'
+alias eqy='command equery keywords $argv'
+alias eql='command equery list $argv'
+alias eqm='command equery meta $argv'
+alias eqs='command equery size $argv'
+alias equ='command equery uses $argv'
+alias eqw='command equery which $argv'
