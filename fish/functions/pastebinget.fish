8242dd78b32382be5b04e33a10b523f9013af6af
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-03-27 23:24:17 +0200

    fish: pastebinget utility

---
+++ fish/functions/pastebinget.fish
@@ -0,0 +1,8 @@
+function pastebinget --description "download a pastebin"
+    if not set -q argv[1] || not set -q argv[2]
+        echo "usage: pastebinget [PASTEBINCODE] <OUTPUTNAME>"
+        return 1
+    end
+    
+    wget https://pastebin.com/raw/$argv[1] -O $argv[2]
+end
