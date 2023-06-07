2cfa50e7c0eba6cdcea554470de8edeb684074c7
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-06-07 02:01:03 +0200

    [fish] correctly ignore gentoo aliases on non-gentoo systems

--- fish/conf.d/gentoo.fish
+++ fish/conf.d/gentoo.fish
@@ -1,24 +1,22 @@
-if not command -v emerge 2&> /dev/null
-    return
+if command -v emerge 2&> /dev/null
+    # equery
+    alias eq='command equery'
+    alias eqb='command equery belongs'
+    alias eqc='command equery changes'
+    alias eqk='command equery check'
+    alias eqd='command equery depends'
+    alias eqg='command equery depgraph'
+    alias eqf='command equery files'
+    alias eqa='command equery has'
+    alias eqh='command equery hasuse'
+    alias eqy='command equery keywords'
+    alias eql='command equery list'
+    alias eqm='command equery meta'
+    alias eqs='command equery size'
+    alias equ='command equery uses'
+    alias eqw='command equery which'
+
+    # eix
+    alias eixi='command eix --installed'
+    alias eixo='command eix --installed-from-overlay'
 end
-
-# equery
-alias eq='command equery'
-alias eqb='command equery belongs'
-alias eqc='command equery changes'
-alias eqk='command equery check'
-alias eqd='command equery depends'
-alias eqg='command equery depgraph'
-alias eqf='command equery files'
-alias eqa='command equery has'
-alias eqh='command equery hasuse'
-alias eqy='command equery keywords'
-alias eql='command equery list'
-alias eqm='command equery meta'
-alias eqs='command equery size'
-alias equ='command equery uses'
-alias eqw='command equery which'
-
-# eix
-alias eixi='command eix --installed'
-alias eixo='command eix --installed-from-overlay'
