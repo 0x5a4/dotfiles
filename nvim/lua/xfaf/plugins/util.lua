a2a1fce859cd4d4f6fca89778ec07d5afe4f6002
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-08-02 11:55:32 +0200

    [nvim] nvim-spider for better web navigation

--- nvim/lua/xfaf/plugins/util.lua
+++ nvim/lua/xfaf/plugins/util.lua
@@ -139,5 +139,15 @@ return {
         },
         cmd = { "HexDump", "HexAssemble", "HexToggle" },
         config = true,
+    },
+    {
+        "chrisgrieser/nvim-spider",
+        keys = {
+            { "w",  "<cmd>lua require('spider').motion('w')<CR>",  mode = { "n", "o", "x" } },
+            { "e",  "<cmd>lua require('spider').motion('e')<CR>",  mode = { "n", "o", "x" } },
+            { "b",  "<cmd>lua require('spider').motion('b')<CR>",  mode = { "n", "o", "x" } },
+            { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+        }
     }
+
 }
