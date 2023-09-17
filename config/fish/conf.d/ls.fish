7e82652649b1187e120f7ff3037d35e0898e8606
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-09-17 17:49:59 +0200

    [fish] ls cleanup

--- config/fish/conf.d/ls.fish
+++ config/fish/conf.d/ls.fish
@@ -1,17 +1,39 @@
-if command -v exa -v eza &> /dev/null 
-    set -l exaimpl $(command -v exa || command -v eza)
+set -l shared_opts -F --sort extension 
+
+set -l exaopts -n --no-user --group --group-directories-first --git --icons
+set -l ezaopts -Mo --hyperlink --git-repos-no-status
+
+set -l opts $shared_opts
+
+
+if set ls_impl $(command -v eza)
+    set exa_available
+
+    set -a opts $exaopts $ezaopts
+else if set ls_impl $(command -v exa)
+    set exa_available
     
-    # -F show classify symbols
-    # -M mount details
-    # -n use numeric user/group ids
-    # -o octal permissions
-    alias 'ls'="$exaimpl -FMno --no-user --color=auto --hyperlink --sort extension --git --git-repos-no-status --group-directories-first --icons"
+    set -a opts $exaopts
+else
+    # neither exa nor eza are installed, so we stick with ls
+    # 
+    # -h for human readable numbers(exa/eza default)
+    #
+    # exa doesnt support hyperlinks so it cant be shared
+    set ls_impl ls
+    set -a opts -h --hyperlink=auto
+end
+
+alias 'ls'="$ls_impl $opts"
+
+# if exa/eza are available, we can use them as tree
+if set -q exa_available
+    set -e exa_available
+    
     alias 'gls'='ls -l --git-ignore'
     alias 'tree'='ls --tree'
     alias 'ltree'='tree -l'
     alias 'gtree'='tree -l --git-ignore'
-else
-    alias 'ls'='command ls -h --classify=auto --color=auto --hyperlink=auto --sort extension'
 end
 
 alias 'l'='ls -l'
