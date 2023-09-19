5a0634923c449e4e9e687d0015ea61a8e52c22e0
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-09-19 02:26:36 +0200

    [fish] fix ls(for real this time)

--- config/fish/conf.d/ls.fish
+++ config/fish/conf.d/ls.fish
@@ -1,10 +1,15 @@
-set -l shared_opts -F --sort extension 
+# options that eza, exa and ls accept
+set -l shared_opts -F --sort extension
+
+# exa's -a is -A in practice so it doesnt support -A. eza correctly
+# understands them as being the same. since ls is still ls,
+# we use -A unless its exa, then we use -a :rolling_eyes:
+set -l show_all_opt -A
 
 set -l exaopts -n --no-user --group-directories-first --git --icons
 set -l ezaopts -Mo --hyperlink --git-repos-no-status
 
 set -l opts $shared_opts
-
 
 if set ls_impl $(command -v eza)
     set exa_available
@@ -13,15 +18,16 @@ if set ls_impl $(command -v eza)
 else if set ls_impl $(command -v exa)
     set exa_available
     
+    set show_all_opt -a
     set -a opts $exaopts
 else
     # neither exa nor eza are installed, so we stick with ls
     # 
     # -h for human readable numbers(exa/eza default)
-    #
-    # exa doesnt support hyperlinks so it cant be shared
+    # --hyperlink because exa doesnt support it, cant be shared
+    # --color because ls is fucking ancient
     set ls_impl ls
-    set -a opts -h --hyperlink=auto
+    set -a opts -h --hyperlink=auto --color=auto
 end
 
 alias 'ls'="$ls_impl $opts"
@@ -37,4 +43,5 @@ if set -q exa_available
 end
 
 alias 'l'='ls -l'
-alias 'la'='ls -a'
+alias 'll'='l'
+alias 'la'="ll $show_all_opt"
