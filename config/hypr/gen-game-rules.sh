2a11515b33b9d199b33faeabfa77a39114760551
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-10-07 02:43:20 +0200

    [hyprland] rule cleanup
    rules for games are now generated by a separate script, since
    i can no longer be bothered to copy paste the same regex 4 times.
    i've also noticed that a lot of games no longer just have the
    steam_game_[0-9]* class but their actual name(looking at you Stardew
    Valley).

---
+++ config/hypr/gen-game-rules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GAME_CLASSES=(
+    "steam_app_[0-9]*"
+    "Stardew Valley"
+    "Minecraft\*? 1.[0-9|.]*"
+    "FTB Skies"
+    "?:Enigmatica 2: Expert \- Extended"
+
+)
+
+if (( $# == 0 )); then
+    echo "expected game workspace as parameter"
+    exit
+fi
+
+GAME_WORKSPACE=$1
+
+for class in "${GAME_CLASSES[@]}"; do
+    hyprctl --batch "\
+        keyword windowrule workspace ${GAME_WORKSPACE},^${class}\$;\
+        keyword windowrule fullscreen,^$class\$; \
+        keyword windowrule idleinhibit always,^$class\$; \
+        keyword windowrule forcergbx,^$class\$; \
+    "
+done
