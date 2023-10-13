7d6e35c6281c093e1eb31d1d513e01c7a44f27e7
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-10-13 19:29:41 +0200

    [fish] override makefile location using env var

--- config/fish/functions/make.fish
+++ config/fish/functions/make.fish
@@ -1,11 +1,18 @@
 function make -w make
-    pushd $PWD
 
+    # preserve original PWD
+    pushd $PWD
+    set -x GNU_MAKE_EXEC_DIR "$PWD"
+
+    # if GNU_MAKE_ROOT_DIR is set, move there
     if set -q GNU_MAKE_ROOT_DIR
         cd $GNU_MAKE_ROOT_DIR
     end
 
+    # execute order 66
     command make $argv
 
+    # cleanup our garbage
+    set -e GNU_MAKE_EXEC_DIR
     popd
 end
