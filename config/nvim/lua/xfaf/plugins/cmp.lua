5e595d46dbb9fa8980b4d401e890ecf6bb4434a2
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-10-15 22:54:13 +0200

    [nvim] fix friendly-snippets

--- config/nvim/lua/xfaf/plugins/cmp.lua
+++ config/nvim/lua/xfaf/plugins/cmp.lua
@@ -128,6 +128,9 @@ return {
     {
         "L3MON4D3/LuaSnip",
         lazy = true,
+        config = function ()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
         dependencies = "rafamadriz/friendly-snippets",
     },
 
