b40e8ee4e8f6b6142bec1f8a9d745fe7d6e9bd3c
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-11-19 15:32:45 +0100

    nvim: proper git bindings

--- config/nvim/lua/xfaf/plugins/util.lua
+++ config/nvim/lua/xfaf/plugins/util.lua
@@ -95,8 +95,18 @@ return {
         "lewis6991/gitsigns.nvim",
         dependencies = "nvim-lua/plenary.nvim",
         event = "User GitFile",
+        cmd = { "Gitsigns" },
         keys = {
-            { "<leader>og", "<cmd>Gitsigns toggle_linehl<CR>" }
+            { "<leader>og",  "<cmd>Gitsigns toggle_linehl<CR>" },
+            { "<leader>gb",  "<cmd>Gitsigns toggle_current_line_blame<CR>" },
+            { "<leader>gx",  "<cmd>Gitsigns toggle_deleted<CR>" },
+            { "<leader>gd",  "<cmd>Gitsigns diffthis<CR>" },
+            { "<leader>gn",  "<cmd>Gitsigns next_hunk<CR>" },
+            { "<leader>gc",  "<cmd>Gitsigns prev_hunk<CR>" },
+            { "<leader>g+",  "<cmd>Gitsigns stage_hunk<CR>" },
+            { "<leader>gs",  "<cmd>Gitsigns stage_buffer<CR>" },
+            { "<leader>g#",  "<cmd>Gitsigns undo_stage_hunk<CR>" },
+            { "<leader>grr", "<cmd>Gitsigns reset_hunk<CR>" },
         },
         opts = {
             signcolumn = false,
