9100edcbc184420f3865cbbf83b249c8bdfcd766
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-12-08 20:01:35 +0100

    nix fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,13 +1,24 @@
 {
   description = "Ein Satz mit X";
 
+  inputs = {
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+
   outputs = {
     self,
     nixpkgs,
+    nixos-hardware,
+    sops-nix,
   }: {
     nixosConfigurations.t420 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [./t420];
+      modules = [
+        ./t420
+        nixos-hardware.nixosModules.lenovo-thinkpad-t420
+        sops-nix.nixosModules.sops
+      ];
     };
   };
 }
---
+++ nixos/t420/default.nix
@@ -0,0 +1,93 @@
+{
+  config,
+  pkgs,
+  sops,
+  ...
+}: {
+  nix.settings.experimental-features = "nix-command flakes";
+
+  sops.age.keyFile = "/home/notuser/.config/sops/age/keys.txt";
+
+  imports = [
+    ./hardware-configuration.nix
+    ../share/desktop.nix
+    ../share/wifi.nix
+  ];
+
+  # Bootloader.
+  boot.loader.timeout = 1;
+  boot.loader.systemd-boot.editor = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  networking.hostName = "t420";
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-01c4abed-ca86-434d-a319-e2a1747fd239".device = "/dev/disk/by-uuid/01c4abed-ca86-434d-a319-e2a1747fd239";
+  boot.initrd.luks.devices."luks-01c4abed-ca86-434d-a319-e2a1747fd239".keyFile = "/crypto_keyfile.bin";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.notuser = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    description = "no";
+    extraGroups = ["wheel"];
+    packages = with pkgs; [];
+  };
+  users.defaultUserShell = pkgs.fish;
+  programs.ssh.startAgent = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.neovim.withPython3 = true;
+  programs.neovim.viAlias = true;
+
+  programs.fish.enable = true;
+
+  environment.shellAliases = {
+    ls = null;
+    ll = null;
+    l = null;
+  };
+
+  environment.systemPackages = with pkgs; [
+    alejandra
+    nodejs_20
+    gcc
+    clang
+    acpi
+  ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "23.11";
+}
