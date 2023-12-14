88e56ba9df98083da8226b6a84f874134f63c6d0
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-12-14 23:44:28 +0100

    nixos(fword): external monitor waybar

--- nixos/home-manager/waybar.nix
+++ nixos/home-manager/waybar.nix
@@ -3,34 +3,51 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  sharedModules = builtins.fromJSON (builtins.readFile ../../config/waybar/shared-modules.json);
+in {
   programs.waybar = {
     enable = true;
     settings = {
-      mainBar = {
-        output = "eDP-1";
-        layer = "top";
-        position = "top";
-        exclusive = true;
-        passtrough = false;
-        height = 32;
-        modules-left = [
-          "clock"
-          "hyprland/workspaces"
-          "memory"
-          "cpu"
-          "network"
-          "bluetooth"
-        ];
-        modules-right = [
-          "hyprland/submap"
-          "battery"
-          "backlight"
-          "custom/mako"
-          "wireplumber"
-          "user"
-        ];
-      } // (builtins.fromJSON (builtins.readFile ../../config/waybar/shared-modules.json));
+      mainBar =
+        {
+          output = "eDP-1";
+          layer = "top";
+          position = "top";
+          height = 32;
+          modules-left = [
+            "clock"
+            "hyprland/workspaces"
+            "memory"
+            "cpu"
+            "network"
+            "bluetooth"
+          ];
+          modules-right = [
+            "hyprland/submap"
+            "battery"
+            "backlight"
+            "custom/mako"
+            "wireplumber"
+            "user"
+          ];
+        }
+        // sharedModules;
+      fallback =
+        {
+          layer = "top";
+          output = "!eDP-1";
+          position = "top";
+          height = 32;
+          modules-left = [
+            "clock"
+          ];
+          modules-right = [
+            "hyprland/submap"
+            "battery#standalone"
+          ];
+        }
+        // sharedModules;
     };
   };
 }
