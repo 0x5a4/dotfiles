f9c00b2b8e21de9f1dd6029b90ab625deeb94735
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-12-19 16:02:28 +0100

    nvim: fix markdown preview

--- config/nvim/lua/xfaf/plugins/util.lua
+++ config/nvim/lua/xfaf/plugins/util.lua
@@ -61,9 +61,11 @@ return {
     },
     {
         "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
         keys = {
-            { "<leader>vm", "<Plug>MarkdownPreviewToggle" }
+            { "<leader>vm", "<cmd>MarkdownPreviewToggle<CR>" }
         }
     },
     {
