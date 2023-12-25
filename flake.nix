e438bb833f24c724d6a62c15df293f21e83b2d09
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-12-25 13:48:05 +0100

    nix: merge flakes

--- flake.nix
+++ flake.nix
@@ -4,33 +4,71 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
+    nixos-hardware,
+    sops-nix,
+    home-manager,
+    firefox-addons,
+  }: let
+    sharedModules = [
+      sops-nix.nixosModules.sops
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.notuser = import ./nixos/home-manager/home.nix;
+        home-manager.extraSpecialArgs = {inherit inputs;};
+      }
+    ];
+
+    devShell = flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            rcm
             gnumake
-            sops
+            rcm
             git
           ];
         };
-
-        apps = rec {
-          make = flake-utils.lib.mkApp {
-            drv = pkgs.gnumake;
-            exePath = "/bin/make";
-          };
-          default = make;
-        };
       }
     );
+  in {
+    nixosConfigurations.t420 = nixpkgs.lib.nixosSystem {
+      modules =
+        [
+          ./nixos/t420
+          nixos-hardware.nixosModules.lenovo-thinkpad-t420
+        ]
+        ++ sharedModules;
+    };
+
+    nixosConfigurations.fword = nixpkgs.lib.nixosSystem {
+      modules =
+        [
+          ./nixos/fword
+          nixos-hardware.nixosModules.framework-13th-gen-intel
+        ]
+        ++ sharedModules;
+    };
+  } // devShell;
 }
