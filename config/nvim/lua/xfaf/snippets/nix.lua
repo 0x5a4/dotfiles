a72cee75ef66e38f5d5256c704c4729ee5528ac4
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-12-28 19:59:35 +0100

    nvim: nix snippets

---
+++ config/nvim/lua/xfaf/snippets/nix.lua
@@ -0,0 +1,20 @@
+return {
+    s(
+        "with",
+        fmt([[
+            with {}; {};
+        ]], {
+           i(1), i(2) 
+        })
+    ),
+    s(
+        "withpkg",
+        fmt([[
+            with pkgs; [
+                {}
+            ];
+        ]], {
+            i(1)
+        })
+    ),
+}
