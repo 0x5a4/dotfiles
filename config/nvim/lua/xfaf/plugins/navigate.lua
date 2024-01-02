2d3b864a7ff4430161c81b07ef969227d2de1c32
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-01-02 19:31:49 +0100

    nvim: replace leap with flash

--- config/nvim/lua/xfaf/plugins/navigate.lua
+++ config/nvim/lua/xfaf/plugins/navigate.lua
@@ -29,18 +29,27 @@ return {
         event = "User VisualEnter"
     },
     {
-        "ggandor/leap.nvim",
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
         keys = {
-            { "s",  "<Plug>(leap-forward-to)",  mode = { "n", "x", "o" } },
-            { "S",  "<Plug>(leap-backward-to)", mode = { "n", "x", "o" } },
-            { "gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" } },
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end, },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, },
+            { "r",     mode = "o",               function() require("flash").remote() end, },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end, },
+            {
+                "gs",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump({
+                        search = { mode = "search", max_length = 0 },
+                        label = { after = { 0, 0 } },
+                        pattern = "^"
+                    })
+                end
+            },
         },
-        config = function()
-            require('leap').add_repeat_mappings(';', ',', {
-                relative_directions = true,
-                modes = { 'n', 'x', 'o' },
-            })
-        end
     },
     {
         "christoomey/vim-tmux-navigator",
