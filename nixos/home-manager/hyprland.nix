5d897f2dd581a17047679fdf5628a71b44369457
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-02-23 15:38:23 +0100

    hypr: fscs monitors change their serial number

--- nixos/home-manager/hyprland.nix
+++ nixos/home-manager/hyprland.nix
@@ -19,11 +19,13 @@
       $monitorName = eDP-1
       $fscsright = Dell Inc. DELL P2715Q V48W271PA8VL
       $fscsleft = Dell Inc. DELL P2715Q V48W271PA4BL
+      $fscsother = Dell Inc. DELL P2715Q V48W271PA4CL
       $fscsbeamer = Acer Technologies H6510BD JFZ110018401
 
       monitor = $monitorName, preferred, 0x0, 1.333333
       monitor = desc:$fscsright, preferred, 0x-1440, 1.5
       monitor = desc:$fscsleft, preferred, 0x-1440, 1.5
+      monitor = desc:$fscsother, preferred, 0x-1440, 1.5
       monitor = desc:$fscsbeamer, preferred, 0x-1080, 1
 
       # bind workspaces
