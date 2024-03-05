150fdbded217dfa2f320715589e575c4aaf0c794
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-03-05 11:03:47 +0100

    nix: update flake.lock

--- nixos/share/desktop.nix
+++ nixos/share/desktop.nix
@@ -71,16 +71,16 @@
     pulse.enable = true;
   };
 
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
       bluez_monitor.properties = {
       	["bluez5.enable-sbc-xq"] = true,
       	["bluez5.enable-msbc"] = true,
       	["bluez5.enable-hw-volume"] = true,
       	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
       }
-    '';
-  };
+    '')
+  ];
 
   # swaylock pam module
   security.pam.services.swaylock = {
