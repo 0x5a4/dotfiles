085d0377d27de96eaa31beada63f7871b52774ad
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-03-20 00:32:00 +0100

    nvim: dont auto format on save because it produces bloated diffs

--- config/nvim/lua/xfaf/plugins/lsp.lua
+++ config/nvim/lua/xfaf/plugins/lsp.lua
@@ -28,23 +28,6 @@ return {
                     }),
                 },
                 border = "rounded",
-                on_attach = function(client, bufnr)
-                    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-                    if client.supports_method("textDocument/formatting") then
-                        vim.api.nvim_clear_autocmds({
-                            group = augroup,
-                            buffer = bufnr,
-                        })
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            group = augroup,
-                            buffer = bufnr,
-                            callback = function()
-                                vim.lsp.buf.format({ bufnr = bufnr })
-                            end,
-                        })
-                    end
-                end,
             })
         end
     },
