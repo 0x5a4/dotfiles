17873353f16e318f653ad291eb40f3d8096e7154
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-04-08 20:28:58 +0200

    nix: home manager session vars actually work now

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -84,3 +84,12 @@ alias 'ga'='git add'
 alias 'gl'='git lg'
 alias 'gaa'='git add --all'
 alias 'gcm'='git commit -m'
+
+set -l HM_SESSION_VARS_SYSTEM "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
+set -l HM_SESSION_VARS_USER "~/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+for path in $HM_SESSION_VARS_SYSTEM $HM_SESSION_VARS_USER
+    if [ -f $path ]
+        fenv source $path
+    end
+end
