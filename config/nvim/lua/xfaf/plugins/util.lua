773a0e1d039a3acfbdd9ac91bf77aa0706f6087f
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-04-23 13:37:21 +0200

    nvim: tweak what vimtex conceals

--- config/nvim/lua/xfaf/plugins/util.lua
+++ config/nvim/lua/xfaf/plugins/util.lua
@@ -54,6 +54,21 @@ return {
             vim.g.vimtex_view_method = "zathura"
             vim.g.vimtex_quickfix_mode = 0
             vim.g.tex_conceal = "abdmg"
+            vim.g.vimtex_syntax_conceal = {
+                accents = 1,
+                cites = 1,
+                fancy = 1,
+                greek = 1,
+                ligatures = 1,
+                math_delimiters = 1,
+                math_super_sub = 1,
+                math_symbols = 1,
+                spacing = 1,
+                styles = 1,
+                math_bounds = 0,
+                math_fracs = 0,
+                sections = 0,
+            }
         end,
         lazy = false,
         keys = {
