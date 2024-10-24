efb68a67f18e90e5dcd31ee022c32a21fe2bcc2e
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-10-24 22:11:30 +0200

    update flake.lock

--- mod/home-manager/desktop/wob.nix
+++ mod/home-manager/desktop/wob.nix
@@ -6,25 +6,25 @@
   options.xfaf.desktop.wob.enable = lib.mkEnableOption "install 0x5a4s wob config";
 
   config = lib.mkIf config.xfaf.desktop.wob.enable {
-    home.file = {
-      ".config/wob/wob.ini".text = ''
-        orientation = vertical
-        anchor = left
-        width = 50
-        height = 300
-        margin = 20
-
-        [style.muted]
-        bar_color = 44475a
-
-        [style.brightness]
-        border_color = f1fa8c
-      '';
-    };
-
     services.wob = {
       enable = true;
       systemd = true;
+    };
+
+    services.wob.settings = {
+      "" = {
+        orientation = "vertical";
+        anchor = "left";
+        width = 50;
+        height = 300;
+        margin = 20;
+      };
+      "style.muted" = {
+        bar_color = "44475a";
+      };
+      "style.brightness" = {
+        bar_color = "f1fa8c";
+      };
     };
   };
 }
--- overlays.nix
+++ overlays.nix
@@ -17,20 +17,5 @@
           (oldAttrs.patches or [])
           ++ [./config/hyprland_splashes.patch];
       });
-
-    tlp = prev.tlp.overrideAttrs (old: {
-      makeFlags =
-        (old.makeFlags or [])
-        ++ [
-          "TLP_ULIB=/lib/udev"
-          "TLP_NMDSP=/lib/NetworkManager/dispatcher.d"
-          "TLP_SYSD=/lib/systemd/system"
-          "TLP_SDSL=/lib/systemd/system-sleep"
-          "TLP_ELOD=/lib/elogind/system-sleep"
-          "TLP_CONFDPR=/share/tlp/deprecated.conf"
-          "TLP_FISHCPL=/share/fish/vendor_completions.d"
-          "TLP_ZSHCPL=/share/zsh/site-functions"
-        ];
-    });
   };
 }
