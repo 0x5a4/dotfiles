a97b4d70b3e3de82f61b42c9dc8515cea23c7705
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-11-20 11:19:07 +0100

    silence ssh-agent startup on droid

--- host/droid/home.nix
+++ host/droid/home.nix
@@ -30,7 +30,7 @@
   };
 
   programs.fish.interactiveShellInit = ''
-    pidof ssh-agent || eval (ssh-agent -c)
+    pidof ssh-agent &> /dev/null || eval (ssh-agent -c) &> /dev/null
   '';
 
   home.stateVersion = "24.05";
